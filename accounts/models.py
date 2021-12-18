51c1263b4aa52f6afcaa77734bbfc32d9a64341f
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-12-18 14:39:26 +0300

    Pre_save and post_save

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,36 @@ from django.db import models
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import (
-post_save
+post_save,
+pre_save
 )
 User=settings.AUTH_USER_MODEL
+@receiver(pre_save,sender=User) 
+def user_pre_save_receiver(sender,instance,*args,**kwargs):
+    """
+    Before saved in the database
+    """
+    print(instance.username,instance.id) #None
+
+    #trigger pre_save
+    #Avoid this instance.save()
+    #Causes a maximum recurtion error
+    #trigger post_save
+# pre_save.connect(user_created_handler,sender=User)
+
+
+
+@receiver(post_save,sender=User)
+def user_post_save_receiver(sender,instance,created,*args,**kwargs):
+    """
+    After saved in the database
+    """
+    if created:
+        print("Send email to",instance.username)
+        #trigger pre_save
+        # instance.save()
+        #trigger post_save
+    else:
+        print(instance.username,"was just saved")
+
+# post_save.connect(user_created_handler,sender=User)
